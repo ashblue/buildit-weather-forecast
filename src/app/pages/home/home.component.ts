0bcbc62ddd4b8a3b9e453da7ea9c5630abfe678b
Ash Blue <ash@blueashes.com>
2017-11-06 16:06:45 -0700

    Home page now acts as a controller to handle queries

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GeolocationService } from '../../services/geolocation/geolocation.service';
+import { ModelForecast } from '../../models/forecast/forecast';
+import { WeatherService } from '../../services/weather/weather.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  errorMessage: string;
+  loading: boolean;
+  forecast: ModelForecast;
 
-  constructor() { }
+  constructor(
+    private geolocationService: GeolocationService,
+    private weatherService: WeatherService
+  ) { }
 
   ngOnInit() {
+    this.loadBegin();
+
+    this.geolocationService.findCurrentLocation()
+      .then((coordinates) => {
+        this.searchByCoordinates(coordinates);
+      }, (err) => {
+        this.loadEnd();
+      });
   }
 
+  searchByCity(city: string) {
+    this.loadBegin();
+
+    this.weatherService.getDailyForecastByCity(city)
+      .then(this.forecastSuccess, this.forecastError);
+  }
+
+  searchByZipCode(zipCode: string) {
+    this.loadBegin();
+
+    this.weatherService.getDailyForecastByZip(zipCode)
+      .then(this.forecastSuccess, this.forecastError);
+  }
+
+  searchByCoordinates(coordinates: any) {
+    this.loadBegin();
+
+    this.weatherService.getDailyForecastByCoordinates(coordinates.coords.latitude, coordinates.coords.longitude)
+      .then(this.forecastSuccess, this.forecastError);
+  }
+
+  /**
+   * Promise success promise handling
+   * @param forecast
+   */
+  private forecastSuccess = (forecast) => {
+    this.forecast = forecast;
+    this.loadEnd();
+  };
+
+  /**
+   * Forecast error promise handling
+   * @param err
+   */
+  private forecastError = (err) => {
+    this.errorMessage = err;
+    this.loadEnd();
+  };
+
+  /**
+   * Trigger when an async load has started
+   */
+  private loadBegin() {
+    this.loading = true;
+    this.forecast = null;
+    this.errorMessage = null;
+  }
+
+  /**
+   * Trigger when an async load has completed
+   */
+  private loadEnd() {
+    console.log(this.forecast);
+    this.loading = false;
+  }
 }
