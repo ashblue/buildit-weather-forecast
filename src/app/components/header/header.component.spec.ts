e40dac33e650b9d285824652f5b2b93d58017e10
Ash Blue <ash@blueashes.com>
2017-11-06 19:20:32 -0700

    Fixed broken component tests

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { SearchLocationComponent } from '../search-location/search-location.component';
+import { FormsModule } from '@angular/forms';
+import { GeolocationService } from '../../services/geolocation/geolocation.service';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,9 +11,17 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      imports: [
+        FormsModule
+      ],
+      declarations: [
+        SearchLocationComponent,
+        HeaderComponent
+      ],
+      providers: [
+        GeolocationService
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/components/search-location/search-location.component.spec.ts
+++ src/app/components/search-location/search-location.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchLocationComponent } from './search-location.component';
+import { FormsModule } from '@angular/forms';
+import { GeolocationService } from '../../services/geolocation/geolocation.service';
 
 describe('SearchLocationComponent', () => {
   let component: SearchLocationComponent;
@@ -8,7 +10,15 @@ describe('SearchLocationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchLocationComponent ]
+      imports: [
+        FormsModule
+      ],
+      declarations: [
+        SearchLocationComponent
+      ],
+      providers: [
+        GeolocationService
+      ]
     })
     .compileComponents();
   }));
